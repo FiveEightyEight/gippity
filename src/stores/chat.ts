f8c1f3263aec6da444953e9309211dd19106af7f
FiveEightyEight <robert.abreu3@gmail.com>
2024-08-26 22:14:12 -0400

    add setter and getter for chatid

--- src/stores/chat.ts
+++ src/stores/chat.ts
@@ -3,3 +3,11 @@ import { atom } from 'nanostores';
 export type ChatID = string;
 
 export const chatId = atom<ChatID | null>(null);
+
+export function setChatId(id: ChatID | null) {
+    chatId.set(id);
+}
+
+export function getChatId(): ChatID | null {
+    return chatId.get();
+}
