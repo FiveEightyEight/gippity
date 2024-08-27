da56326e61ccc9a44326f8c1323b74ec765319ec
FiveEightyEight <robert.abreu3@gmail.com>
2024-08-27 17:09:26 -0400

    render model drop down, maintain state

--- src/components/ModelSelector.tsx
+++ src/components/ModelSelector.tsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
-import type { Model } from './types';
-
+import { useStore } from '@nanostores/react';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
+import { $selectedModel, setSelectedModel } from '../stores/chat';
+import type { Model } from './types';
+
+
 const ModelSelector = (props: { models: Model[] }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [modelName, setModelName] = useState("");
+    const selectedModel = useStore($selectedModel);
+    
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -18,7 +24,7 @@ const ModelSelector = (props: { models: Model[] }) => {
                     onClick={toggleMenu}
                     className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                 >
-                    Options
+                    {selectedModel && modelName || "Select Model"}
                     <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
                 </MenuButton>
             </div>
@@ -28,35 +34,21 @@ const ModelSelector = (props: { models: Model[] }) => {
                     className="absolute left-0 z-10 mt-2 w-56 origin-top rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                 >
                     <div className="py-1">
-                        <MenuItem>
-                            <button
-                                className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                            >
-                                Account settings
-                            </button>
-                        </MenuItem>
-                        <MenuItem>
-                            <button
-                                className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                            >
-                                Support
-                            </button>
-                        </MenuItem>
-                        <MenuItem>
-                            <button
-                                className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                            >
-                                License
-                            </button>
-                        </MenuItem>
-                        <MenuItem>
-                            <button
-                                type="submit"
-                                className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                            >
-                                Sign out
-                            </button>
-                        </MenuItem>
+                        {props.models.map((model) => (
+                            <MenuItem key={model.id}>
+                                <button
+                                    className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                                    onClick={() => {
+                                        setIsOpen(false)
+                                        setModelName(model.name)
+                                        setSelectedModel(model.version)
+                                    }}
+                                >
+                                    {model.name}
+                                </button>
+                            </MenuItem>
+                        ))}
+
                     </div>
                 </MenuItems>
             )}
