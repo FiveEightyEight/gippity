d1827a0d99ffac0b7f596e3e3551a9be2d997118
FiveEightyEight <robert.abreu3@gmail.com>
2024-09-27 13:58:58 -0400

    created icon component sub-directory

---
+++ src/components/icons/checkmark.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+export default function MaterialSymbolsLightCheck(props: SVGProps<SVGSVGElement>) {
+    return (<svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m9.55 17.308l-4.97-4.97l.714-.713l4.256 4.256l9.156-9.156l.713.714z"></path></svg>);
+}
